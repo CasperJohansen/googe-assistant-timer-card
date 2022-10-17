4f2c5daf7c31de7a1f424096f70cdf27f4e5126a
Casper Johansen <casper2001@live.dk>
2022-10-18 00:33:59 +0200

    Working state

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # googe-assistant-timer-card
+
+Custom card for Home Assistant.
+
 Shows current timers set on google home devices
+
+Note: i cant get it to update every second - it updates at random, but acceptable intervals imo.
+
+Note2: If no timer is set, the card will be very thin as to not disturb your layout.
+
+(HACS Install):
+    Before you install this:
+    Download and setup Google Home Integration (https://github.com/leikoilja/ha-google-home#sensors) 
+    You can find it in the default HACS repo. Just search Google Home.
+    Once installed, click on "configure" and change update interval to 30 seconds
+
+    To install this:
+    -> goto HACS(tab) 
+    -> click Frontend 
+    -> click 3 dots in the right corner 
+    -> click custom repositories
+    -> paste 'CasperJohansen/googe-assistant-timer-card' into 'repository' field
+    -> select category and choose Lovelace
+    -> add repository
+    -> click '+ Explore & Download Repositories'
+    -> search for 'Google Assistant Timer Card'
+    -> Download it
+
+    To use this:
+    -> add manual card
+    -> paste:
+        type: custom:ga-timer-card
+        entity: sensor.*YOUR GOOGLE DEVICE*_timers
+    -> enjoy ;)
--- ga-tc.js
+++ ga-tc.js
@@ -1,124 +1,155 @@
+/*
+This project is very unfinished.
 
+In its current state, it updates on its own schedule, i cannot solve the puzzle of how to make it update every second.
+The code is confirmed and working in codepen.
+
+It is use-able in its current state IMO - you are welcome to try and finish it.
+https://github.com/CasperJohansen/google-assistant-timer-card
+*/
 
 
 class GoogleAssistantTimerCard extends HTMLElement {
-    constructor(){
+    constructor() {
         super();
         this.attachShadow({
             mode: 'open'
         });
     }
-
     set hass(hass) {
         const config = this.config;
         const entityId = config.entity;
 
         if(entityId){
             const state = hass.states[entityId];
-            const timers = state.attributes.timers;
-            if(timers != "undefined"){
-                for (const timer of timers) {
-                    if (timer == "Unavailable") {
-                        console.log("Timer unavalible");
-                    } else {
-                        var _timers = [new Date(timer.local_time)];
+            const inputtimers = state.attributes.timers;
+            var i = 1;
+            var timercontainer = [];
+            for (const timer of inputtimers){
+                timercontainer += `
+                    <div class="clockdiv">
+                        <h1 class="timer_title">Timer ${i}</h1>
+                        <div>
+                            <span class="hours${i}"></span>
+                            <div class="smalltext">Hours</div>
+                        </div>
+                        <div>
+                            <span class="minutes${i}"></span>
+                            <div class="smalltext">Minutes</div>
+                        </div>
+                        <div>
+                            <span class="seconds${i++}"></span>
+                            <div class="smalltext">Seconds</div>
+                        </div>
+                    </div>
+                    `;
+            }
+            this.shadowRoot.getElementById('container').innerHTML = timercontainer;
+            var i = 1;
+            for (const timer of inputtimers){
+                var deadline = new Date(timer.local_time_iso);
+                initializeClock(this.shadowRoot, i++, deadline);
+            }
+            function getTimeRemaining(endtime) {
+                const total = Date.parse(endtime) - Date.parse(new Date());
+                const seconds = Math.floor((total / 1000) % 60);
+                const minutes = Math.floor((total / 1000 / 60) % 60);
+                const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
+                return {
+                    total,
+                    hours,
+                    minutes,
+                    seconds
+                };
+                }
+            function initializeClock(shadowRoot, i, endtime) {
+                function updateClock(shadowRoot) {
+                    var hourSpan = shadowRoot.querySelector('.hours' + i)
+                    var minuteSpan = shadowRoot.querySelector('.minutes' + i)
+                    var secondSpan = shadowRoot.querySelector('.seconds' + i)
+                    const t = getTimeRemaining(endtime);
+                    hourSpan.innerHTML = ("0" + t.hours).slice(-2);
+                    minuteSpan.innerHTML = ("0" + t.minutes).slice(-2);
+                    secondSpan.innerHTML = ("0" + t.seconds).slice(-2);
+                    if (t.total <= 0) {
+                    clearInterval(timeinterval);
                     }
                 }
+                updateClock(shadowRoot, i);
+                const timeinterval = setInterval(updateClock(shadowRoot, i), 1000);
             }
         }
-        console.log(_timers);
     }
     setConfig(config) {
+
         const root = this.shadowRoot;
         if (root.lastChild) root.removeChild(root.lastChild);
 
         this.config = config;
-
+        
         const card = document.createElement('ha-card');
         const content = document.createElement('div');
         const style = document.createElement('style')
 
         style.textContent = `
-            .container {
-                padding: 5px 5px 5px;
-                display:flex;
-                flex-flow: row wrap;
-                justify-content: space-around;
-                align-items: flex-start;
-            }
-            .clock {
-                
-                padding: 5px 5px 5px 0px;
-                margin: auto;
-            }
-            .other_clocks {
-                float: right;
-                margin: auto;
-            }
-            .otime {
-                padding: 0px 5px 2px;
-                font-size: 14px;
-                font-family: var(--paper-font-headline_-_font-family);
-                letter-spacing: var(--paper-font-headline_-_letter-spacing);
-                text-rendering: var(--paper-font-common-expensive-kerning_-_text-rendering);
-            }
-            .tz_locale {
-                padding: 0px 5px 1px;
-                color: var(--secondary-text-color);
-                font-size: 11px;
-                font-family: var(--paper-font-headline_-_font-family);
-                letter-spacing: var(--paper-font-headline_-_letter-spacing);
-                text-rendering: var(--paper-font-common-expensive-kerning_-_text-rendering);
-            }     
-            .time {
-                padding: 
-                font-family: var(--paper-font-headline_-_font-family);
-                -webkit-font-smoothing: var(--paper-font-headline_-_-webkit-font-smoothing);
-                font-size: 56px;
-                font-weight: var(--paper-font-headline_-_font-weight);
-                letter-spacing: var(--paper-font-headline_-_letter-spacing);
-                line-height: 1em;
-                text-rendering: var(--paper-font-common-expensive-kerning_-_text-rendering);
-            }
-            .date {
-                font-family: var(--paper-font-headline_-_font-family);
-                -webkit-font-smoothing: var(--paper-font-headline_-_-webkit-font-smoothing);
-                font-size: 24px;
-                font-weight: var(--paper-font-headline_-_font-weight);
-                letter-spacing: var(--paper-font-headline_-_letter-spacing);
-                line-height: var(--paper-font-headline_-_line-height);
-                text-rendering: var(--paper-font-common-expensive-kerning_-_text-rendering);
-            }          
+        .container {
+            padding: 5px 5px 5px;
+            display:flex;
+            flex-flow: row wrap;
+            justify-content: space-around;
+            align-items: flex-start;
+        }
+        .timer_title{
+            color: #396;
+            font-weight: 100;
+            font-size: 20px;
+            margin: 0px 0px 20px;
+        }
+        
+        .clockdiv{
+            font-family: sans-serif;
+            color: #fff;
+            display: inline-block;
+            font-weight: 100;
+            text-align: center;
+            font-size: 30px;
+            margin-bottom: 20px;
+        }
+        
+        .clockdiv > div{
+            padding: 10px;
+            border-radius: 3px;
+            background: #00BF96;
+            display: inline-block;
+        }
+        
+        .clockdiv div > span{
+            padding: 15px;
+            border-radius: 3px;
+            background: #00816A;
+            display: inline-block;
+        }
+        
+        .smalltext{
+            padding-top: 5px;
+            font-size: 16px;
+        } 
         `;
-    
+
         content.id = "container";
         content.className = "container";
         card.header = config.title;
         card.appendChild(style);
         card.appendChild(content);
-        
         root.appendChild(card);
+
+        if (!config.entity) {
+            throw new Error('You need to define an entity');
+          }
     }
     // The height of the card.
     getCardSize() {
         return 3;
     }
 }
-
 customElements.define('ga-timer-card', GoogleAssistantTimerCard);
-
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////
