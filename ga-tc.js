8df2f161d2fb52cece17563a624fa5ae2f7883d3
Casper Johansen <casper2001@live.dk>
2022-10-17 16:34:22 +0200

    Update ga-tc.js

--- ga-tc.js
+++ ga-tc.js
@@ -19,16 +19,92 @@ class GoogleAssistantTimerCard extends HTMLElement {
             if (stateStr == "Unavailable") {
                 //no timer set
             } else {
-                var _timer_end = new Date(stateStr);
+                var _timer = new Date(stateStr);
             }
         }
 
-        if (_timer_end == "Invalid Date") {
+        if (_timer == "Invalid Date") {
             throw new Error("Invalid date. Ensure its a ISO Date")
         }
 
         console.log(state);
         console.log(stateStr);
+        console.log(_timer);
+    }
+    setConfig(config) {
+        const root = this.shadowRoot;
+        if (root.lastChild) root.removeChild(root.lastChild);
+
+        this.config = config;
+
+        const card = document.createElement('ha-card');
+        const content = document.createElement('div');
+        const style = document.createElement('style')
+
+        style.textContent = `
+            .container {
+                padding: 5px 5px 5px;
+                display:flex;
+                flex-flow: row wrap;
+                justify-content: space-around;
+                align-items: flex-start;
+            }
+            .clock {
+                
+                padding: 5px 5px 5px 0px;
+                margin: auto;
+            }
+            .other_clocks {
+                float: right;
+                margin: auto;
+            }
+            .otime {
+                padding: 0px 5px 2px;
+                font-size: 14px;
+                font-family: var(--paper-font-headline_-_font-family);
+                letter-spacing: var(--paper-font-headline_-_letter-spacing);
+                text-rendering: var(--paper-font-common-expensive-kerning_-_text-rendering);
+            }
+            .tz_locale {
+                padding: 0px 5px 1px;
+                color: var(--secondary-text-color);
+                font-size: 11px;
+                font-family: var(--paper-font-headline_-_font-family);
+                letter-spacing: var(--paper-font-headline_-_letter-spacing);
+                text-rendering: var(--paper-font-common-expensive-kerning_-_text-rendering);
+            }     
+            .time {
+                padding: 
+                font-family: var(--paper-font-headline_-_font-family);
+                -webkit-font-smoothing: var(--paper-font-headline_-_-webkit-font-smoothing);
+                font-size: 56px;
+                font-weight: var(--paper-font-headline_-_font-weight);
+                letter-spacing: var(--paper-font-headline_-_letter-spacing);
+                line-height: 1em;
+                text-rendering: var(--paper-font-common-expensive-kerning_-_text-rendering);
+            }
+            .date {
+                font-family: var(--paper-font-headline_-_font-family);
+                -webkit-font-smoothing: var(--paper-font-headline_-_-webkit-font-smoothing);
+                font-size: 24px;
+                font-weight: var(--paper-font-headline_-_font-weight);
+                letter-spacing: var(--paper-font-headline_-_letter-spacing);
+                line-height: var(--paper-font-headline_-_line-height);
+                text-rendering: var(--paper-font-common-expensive-kerning_-_text-rendering);
+            }          
+        `;
+    
+        content.id = "container";
+        content.className = "container";
+        card.header = config.title;
+        card.appendChild(style);
+        card.appendChild(content);
+        
+        root.appendChild(card);
+    }
+    // The height of the card.
+    getCardSize() {
+        return 3;
     }
 }
 
